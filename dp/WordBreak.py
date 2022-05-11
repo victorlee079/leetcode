1e4fe29c5bb6d9f14bb8c6e0e6c5b43e87f168b5
Victor Lee <victorlee079@gmail.com>
2022-05-11 09:27:12 +0800

    Create WordBreak.py

---
+++ dp/WordBreak.py
@@ -0,0 +1,8 @@
+class Solution:   
+    def wordBreak(self, s, words):
+        d = [False] * len(s)    
+        for i in range(len(s)):
+            for w in words:
+                if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
+                    d[i] = True
+        return d[-1]
