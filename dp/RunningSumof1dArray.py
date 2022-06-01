608fc5bac00a44d2bde73479c4022999b5ab5f35
Victor Lee <victorlee079@gmail.com>
2022-06-01 09:43:38 +0800

    Create RunningSumof1dArray.py

---
+++ dp/RunningSumof1dArray.py
@@ -0,0 +1,7 @@
+# Time - O(n)
+# Space - O(1)
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
