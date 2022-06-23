43f37b3dec5d74770c59e914fa90a9698861e9df
Victor Lee <victorlee079@gmail.com>
2022-06-23 11:35:56 +0800

    Update CourseSchedule.py

--- dfs/CourseSchedule.py
+++ dfs/CourseSchedule.py
@@ -28,3 +28,27 @@ class Solution:
                     return False
         return True
             
+    def canFinishKhan(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        next_courses = defaultdict(list)
+        cnt = [0] * numCourses
+        
+        for a, b in prerequisites:
+            next_courses[b].append(a)
+            cnt[a] += 1
+            
+        ans, stk = [], []
+        for i in range(numCourses):
+            if cnt[i] == 0:
+                stk.append(i)
+                
+        while stk:
+            c = stk.pop(0)
+            ans.append(c)
+            if c not in next_courses:
+                continue
+            for p in next_courses[c]:
+                cnt[p] -= 1
+                if cnt[p] == 0:
+                    stk.append(p)
+
+        return True if len(ans) == numCourses else False
