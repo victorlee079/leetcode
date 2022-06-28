8b5be519fd7dac8934bdb9a6b20c7cf57fb4663a
Victor Lee <victorlee079@gmail.com>
2022-06-28 10:05:08 +0800

    Update MinimumDeletionstoMakeCharacterFrequenciesUnique.py

--- greedy/MinimumDeletionstoMakeCharacterFrequenciesUnique.py
+++ greedy/MinimumDeletionstoMakeCharacterFrequenciesUnique.py
@@ -19,3 +19,28 @@ class Solution:
             seen_frequencies.add(frequency[i])
         
         return delete_count
+    
+    # Time complexity: O(N+Klog⁡K)
+    def minDeletionsHeap(self, s: str) -> int:
+        
+        frequency = [0] * 26
+        # O(N)
+        for char in s:
+            frequency[ord(char) - ord('a')] += 1
+        
+        heap = [-freq for freq in frequency if freq != 0]
+        heapq.heapify(heap)
+        
+        delete_count = 0
+        
+        while len(heap) > 1:
+            top = -heapq.heappop(heap)
+            
+            if top == -heap[0]:
+                top -= 1
+                delete_count += 1
+                # Only push back when the frequency > 0
+                if top > 0:
+                    heapq.heappush(heap, -top)
+                
+        return delete_count
