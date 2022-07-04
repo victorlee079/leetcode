7d720f6d0f417231077356d1d961253fea764387
Victor Lee <victorlee079@gmail.com>
2022-07-04 12:51:06 +0800

    Create Candy.py

---
+++ array/Candy.py
@@ -0,0 +1,16 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        left2right = [1] * n
+        right2left = [1] * n
+        for i in range(1, n):
+            if ratings[i] > ratings[i-1]:
+                left2right[i] = left2right[i-1] + 1
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                right2left[i] = right2left[i+1] + 1
+        ans = 0
+        for i in range(n):
+            ans += max(left2right[i], right2left[i])
+        return ans
+        
