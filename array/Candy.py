09ee775e14c02349f1464db26abb11438ea3fbdc
Victor Lee <victorlee079@gmail.com>
2022-07-04 14:49:53 +0800

    Update Candy.py

--- array/Candy.py
+++ array/Candy.py
@@ -13,4 +13,14 @@ class Solution:
         for i in range(n):
             ans += max(left2right[i], right2left[i])
         return ans
-        
+
+    def candyOneArray(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        left2right = [1] * n
+        for i in range(1, n):
+            if ratings[i] > ratings[i-1]:
+                left2right[i] = left2right[i-1] + 1
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                left2right[i] = max(left2right[i], left2right[i+1] + 1)
+        return sum(left2right)
