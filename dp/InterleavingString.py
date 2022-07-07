a4c34b12940e9da06e5368cf1203d421c136d47b
Victor Lee <victorlee079@gmail.com>
2022-07-07 09:52:38 +0800

    Create InterleavingString.py

---
+++ dp/InterleavingString.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n1, n2 = len(s1), len(s2)
+        if n1 + n2 != len(s3):
+            return False
+        
+        dp = [[False for i in range(n2+1)] for j in range(n1+1)]
+        
+        for i in range(n1+1):
+            for j in range(n2+1):
+                if i == 0 and j == 0:
+                    dp[i][j] = True
+                else:
+                    if i == 0:
+                        dp[i][j] = dp[i][j-1] and s3[j-1] == s2[j-1]
+                    elif j == 0:
+                        dp[i][j] = dp[i-1][j] and s3[i-1] == s1[i-1]
+                    else:
+                        dp[i][j] = (s3[i+j-1] == s1[i-1] and dp[i-1][j]) or (s3[i+j-1] == s2[j-1] and dp[i][j-1])    
+        return dp[-1][-1]
+                    
