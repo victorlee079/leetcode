248dcea86cf88c1b3df4fef7ffde6afb0b6bda69
Victor Lee <victorlee079@gmail.com>
2022-07-08 14:42:07 +0800

    Update PaintHouseIII.py

--- dp/PaintHouseIII.py
+++ dp/PaintHouseIII.py
@@ -1,7 +1,18 @@
 class Solution:
     def minCost(self, houses: List[int], cost: List[List[int]], m: int, n: int, target: int) -> int:
-        # dp[i][j][k] minimum cost where we have k neighborhoods in the first i houses and the i-th house is painted with the color j
+      # dp[i][j][k] minimum cost where we have k neighborhoods in the first i houses and the i-th house is painted with the color j
+            
         dp = [[[0 for k in range(target)] for j in range(n)] for i in range(m)]
+        
+        def findPrevCost(i, j, k):
+            prevCost = dp[i-1][j][k]
+            if k > 0:
+                for l in range(n):
+                    if l == j:
+                        continue
+                    prevCost = min(prevCost, dp[i-1][l][k-1])
+            return prevCost
+            
         for i in range(m):
             for j in range(n):
                 for k in range(target):
@@ -20,15 +31,7 @@ class Solution:
                     else:
                         # Not painted
                         if houses[i] == 0:
-                            prevCost = dp[i-1][j][k]
-                            if k > 0:
-                                # Same color with prev and same group, or
-                                # different color, different group
-                                for l in range(n):
-                                    if l == j:
-                                        continue
-                                    prevCost = min(prevCost, dp[i-1][l][k-1])
-                            dp[i][j][k] = prevCost + cost[i][j]
+                            dp[i][j][k] = findPrevCost(i, j, k) + cost[i][j]
                         else:
                             if j+1 == houses[i]:
                                 prevCost = dp[i-1][j][k]
@@ -37,7 +40,7 @@ class Solution:
                                         if l == j:
                                             continue
                                         prevCost = min(prevCost, dp[i-1][l][k-1])
-                                dp[i][j][k] = prevCost
+                                dp[i][j][k] = findPrevCost(i, j, k)
                             else:
                                 dp[i][j][k] = math.inf
         
