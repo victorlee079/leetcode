c51eb7f2daaf2c07cb1216bbd19670483c284f9a
Victor Lee <victorlee079@gmail.com>
2022-07-11 10:20:55 +0800

    Create JumpGameVI.py

---
+++ dp/JumpGameVI.py
@@ -0,0 +1,9 @@
+class Solution:
+    # O(n^2) TLE
+    def maxResult(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        for i in range(1, n):
+            temp, nums[i] = nums[i], -math.inf
+            for j in range(i-1, max(-1, i-k-1), -1):
+                nums[i] = max(nums[i], temp + nums[j])
+        return nums[-1]
