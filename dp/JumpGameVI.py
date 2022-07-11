3d0ae56345ee6462ce4d604dcf61eef9156888b1
Victor Lee <victorlee079@gmail.com>
2022-07-11 11:14:45 +0800

    Update JumpGameVI.py

--- dp/JumpGameVI.py
+++ dp/JumpGameVI.py
@@ -7,3 +7,19 @@ class Solution:
             for j in range(i-1, max(-1, i-k-1), -1):
                 nums[i] = max(nums[i], temp + nums[j])
         return nums[-1]
+    
+    # Use heapq 
+    # Time O(nlogk)
+    def maxResult(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        heap = [(-nums[0], 0)]
+        for i in range(1, n):
+            temp, nums[i] = nums[i], -math.inf
+            
+            while heap and i-k > heap[0][1]:
+                heapq.heappop(heap)
+                
+            val, index = -heap[0][0], heap[0][1]
+            heapq.heappush(heap, (-temp-val, i))
+            nums[i] = temp + val
+        return nums[-1]
