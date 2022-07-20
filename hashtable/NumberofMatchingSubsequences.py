0b522d2eca22cf127c8b040780fd7357dd99611a
Victor Lee <victorlee079@gmail.com>
2022-07-20 11:23:00 +0800

    Update NumberofMatchingSubsequences.py

--- hashtable/NumberofMatchingSubsequences.py
+++ hashtable/NumberofMatchingSubsequences.py
@@ -1,24 +1,18 @@
-class Node:
-    def __init__(self, word):
-        self.word = word
-        self.index = 0
-
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
         buckets = defaultdict(list)
         for word in words:
             startingChar = word[0]
-            buckets[startingChar].append(Node(word))
+            buckets[startingChar].append(word)
 
         ans = 0
         for c in s:
             currBucket = buckets[c]
             buckets[c] = []
-            for node in currBucket:
-                node.index += 1  # Point to next character of node.word
-                if node.index == len(node.word):
+            for w in currBucket:
+                if len(w) == 1:
                     ans += 1
                 else:
-                    startingChar = node.word[node.index]
-                    buckets[startingChar].append(node)
+                    startingChar = w[1]
+                    buckets[startingChar].append(w[1:])
         return ans
