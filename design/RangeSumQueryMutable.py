602971190afe5e6946ed5effc60fceaf144771ee
Victor Lee <victorlee079@gmail.com>
2022-08-01 10:44:34 +0800

    Update RangeSumQueryMutable.py

--- design/RangeSumQueryMutable.py
+++ design/RangeSumQueryMutable.py
@@ -17,6 +17,7 @@ class NumArray:
         i = index + 1        
         while i <= self.n:
             self.bitree[i] += diff
+            # Last set bit
             i += i & (-i)
 
     def sumRange(self, left: int, right: int) -> int:
@@ -35,3 +36,7 @@ class NumArray:
 # obj = NumArray(nums)
 # obj.update(index,val)
 # param_2 = obj.sumRange(left,right)
+
+# BITree[0] is a dummy node. 
+# BITree[y] is the parent of BITree[x], if and only if y can be obtained by removing the last set bit from the binary representation of x, that is y = x – (x & (-x)).
+# The child node BITree[x] of the node BITree[y] stores the sum of the elements between y(inclusive) and x(exclusive): arr[y,…,x). 
