e63caa4a69b45b77f90ccd72f431f3374c9522f0
Victor Lee <victorlee079@gmail.com>
2022-08-02 10:45:22 +0800

    Update KthSmallestElementinaSortedMatrix.py

--- heap/KthSmallestElementinaSortedMatrix.py
+++ heap/KthSmallestElementinaSortedMatrix.py
@@ -13,3 +13,35 @@ class Solution:
                     if matrix[r][c] < -heap[0]:
                         heapq.heappushpop(heap, -matrix[r][c])
         return -heap[0]
+
+    # Time: O(n^2)
+    # Space: O(1)
+    def kthSmallestBinarySearch(self, matrix: List[List[int]], k: int) -> int:
+        n, lo, hi = len(matrix), matrix[0][0], matrix[-1][-1]
+        
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            
+            cnt = 0
+            ans = lo
+            
+            for r in range(n):
+                for c in range(n):
+                    if matrix[r][c] > mid:
+                        break
+                    else:
+                        cnt += 1
+                        ans = max(ans, matrix[r][c])
+            
+            if cnt == k:
+                return ans
+            
+            if cnt > k:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+                
+        return lo
+                
+            
+                        
