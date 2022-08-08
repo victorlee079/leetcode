54267b6ac04329755654b23da853e5be3dde6e28
Victor Lee <victorlee079@gmail.com>
2022-08-08 12:45:50 +0800

    Update PoorPigs.py

--- math/PoorPigs.py
+++ math/PoorPigs.py
@@ -1,4 +1,4 @@
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
         n = minutesToTest // minutesToDie
-        return math.ceil(math.log(buckets, n+1))
+        return math.ceil(math.log(buckets, n+1)) # log a / log b = logb a
