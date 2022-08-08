9a7d02ac02b305be0445e2fdef4c34496c9e1ff4
Victor Lee <victorlee079@gmail.com>
2022-08-08 11:45:45 +0800

    Create PoorPigs.py

---
+++ math/PoorPigs.py
@@ -0,0 +1,4 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        n = minutesToTest // minutesToDie
+        return math.ceil(math.log(buckets, n+1))
