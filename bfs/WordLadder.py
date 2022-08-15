61cd8899a585c44f9d97d4c25e16a664e6e82c03
Victor Lee <victorlee079@gmail.com>
2022-08-15 11:43:28 +0800

    Update WordLadder.py

--- bfs/WordLadder.py
+++ bfs/WordLadder.py
@@ -18,3 +18,19 @@ class Solution:
             return 0
 
         return bfs(q)
+   
+    def ladderLengthRevision(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordSet = set(wordList)
+        q = deque([(beginWord, 1)])
+        while q:
+            word, cnt = q.popleft()
+            if word == endWord:
+                return cnt
+            for i in range(len(word)):
+                for j in range(26):
+                    newWord = word[:i] + chr(97+j) + word[i+1:]
+                    if newWord in wordSet:
+                        wordSet.remove(newWord)
+                        q.append((newWord, cnt+1))
+        return 0
+        
