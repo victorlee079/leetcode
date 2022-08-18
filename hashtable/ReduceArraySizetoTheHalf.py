826078be68679977a248df0a6484cc2a71d5e25d
Victor Lee <victorlee079@gmail.com>
2022-08-18 09:53:42 +0800

    Update ReduceArraySizetoTheHalf.py

--- hashtable/ReduceArraySizetoTheHalf.py
+++ hashtable/ReduceArraySizetoTheHalf.py
@@ -13,3 +13,23 @@ class Solution:
                 return ans
         return len(ans)
         
+    def minSetSizeBuckets(self, arr: List[int]) -> int:
+        n = len(arr)
+        ans = 0
+        s = n
+        # O(n)
+        counts = Counter(arr)
+        
+        buckets = [0] * (n+1)
+        for item in counts.items():
+            buckets[item[1]] += 1
+        
+        for i in range(n, 0, -1):
+            for j in range(buckets[i]):
+                s -= i
+                ans += 1
+                if s <= n//2:
+                    return ans
+
+        return ans
+        
