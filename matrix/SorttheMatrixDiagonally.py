5249dfc219d63d5ee6268d6ddad7b2bc7720bcd5
Victor Lee <victorlee079@gmail.com>
2022-08-29 16:59:52 +0800

    Update SorttheMatrixDiagonally.py

--- matrix/SorttheMatrixDiagonally.py
+++ matrix/SorttheMatrixDiagonally.py
@@ -27,3 +27,16 @@ class Solution:
             k += 1
         
         return mat
+
+        def diagonalSortClean(self, A):
+            n, m = len(A), len(A[0])
+            d = collections.defaultdict(list)
+            for i in xrange(n):
+                for j in xrange(m):
+                    d[i - j].append(A[i][j])
+            for k in d:
+                d[k].sort(reverse=1)
+            for i in xrange(n):
+                for j in xrange(m):
+                    A[i][j] = d[i - j].pop()
+            return A
