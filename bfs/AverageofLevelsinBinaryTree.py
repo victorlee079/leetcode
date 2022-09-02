46fe050676000748052049fc1ee41efa946b0788
Victor Lee <victorlee079@gmail.com>
2022-09-02 10:53:03 +0800

    Create AverageofLevelsinBinaryTree.py

---
+++ bfs/AverageofLevelsinBinaryTree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        q = deque([root])
+        ans = []
+        while q:
+            total = 0
+            n = len(q)
+            for i in range(n):
+                node = q.popleft()
+                total += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(total / n)
+        return ans
