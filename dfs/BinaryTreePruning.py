bb3bf5c53e265b25fe795106658dec6794cecd21
Victor Lee <victorlee079@gmail.com>
2022-09-06 09:14:10 +0800

    Update BinaryTreePruning.py

--- dfs/BinaryTreePruning.py
+++ dfs/BinaryTreePruning.py
@@ -18,3 +18,17 @@ class Solution:
             return node.val + left_cnt + right_cnt
 
         return root if dfs(root) > 0 else None
+
+    def pruneTreeNoCnt(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(node):
+            if not node:
+                return None
+
+            node.left = dfs(node.left) 
+            node.right = dfs(node.right)
+            if not node.left and not node.right:
+                return None if node.val == 0 else node
+            return node
+        
+        return dfs(root)
+        
