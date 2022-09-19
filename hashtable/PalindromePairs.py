bad86e57e783520abadbe421c436591dade6203b
Victor Lee <victorlee079@gmail.com>
2022-09-19 10:15:27 +0800

    Update PalindromePairs.py

--- hashtable/PalindromePairs.py
+++ hashtable/PalindromePairs.py
@@ -2,13 +2,7 @@ class Solution:
     # O(w*n^2)
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
         def is_palindrome(word):
-            l, r = 0, len(word)-1
-            while l <= r:
-                if word[l] != word[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
+            return word == word[::-1]
         
         ans = []
         
