6e9038ee0374b48673565aea1fd076921d4062fa
Victor Lee <victorlee079@gmail.com>
2022-09-20 10:55:52 +0800

    Create MaximumLengtheofRepeatedSubarray.py

---
+++ dp/MaximumLengtheofRepeatedSubarray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        n1, n2 = len(nums1), len(nums2)
+        dp = [[0] * n2 for _ in range(n1)]
+        for i in range(n1):
+            for j in range(n2):
+                if nums1[i] == nums2[j]:
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = dp[i-1][j-1] + 1
+        return max(max(row) for row in dp)
+        
