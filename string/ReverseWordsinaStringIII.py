60b0d6fffd4f2610ac7acbfd341b71c9a3d0d8f6
Victor Lee <victorlee079@gmail.com>
2022-09-22 09:53:46 +0800

    Update ReverseWordsinaStringIII.py

--- string/ReverseWordsinaStringIII.py
+++ string/ReverseWordsinaStringIII.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Stack
     def reverseWords(self, s: str) -> str:
         stk = []
         ans = []
@@ -11,4 +12,20 @@ class Solution:
                 stk.append(c)
         while stk:
             ans.append(stk.pop())
+        return "".join(ans)
+    
+    # Two pointer
+    def reverseWords2p(self, s: str) -> str:
+        n = len(s)
+        ans = list(s)
+        space = -1
+        for i in range(n+1):
+            if i == n or s[i] == " ":
+                start = space + 1
+                end = i - 1
+                while start < end:
+                    ans[start], ans[end] = s[end], s[start]
+                    start += 1
+                    end -= 1
+                space = i
         return "".join(ans)
