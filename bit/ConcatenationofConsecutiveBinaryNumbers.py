170e3eb46032eb23d212395898de164a6cd6740f
Victor Lee <victorlee079@gmail.com>
2022-09-23 10:04:33 +0800

    Update ConcatenationofConsecutiveBinaryNumbers.py

--- bit/ConcatenationofConsecutiveBinaryNumbers.py
+++ bit/ConcatenationofConsecutiveBinaryNumbers.py
@@ -1,5 +1,11 @@
 class Solution:
-    def concatenatedBinary(self, n: int) -> int:
+    def concatenatedBinaryRecur(self, n: int) -> int:
         if n == 1:
             return 1
         return ((self.concatenatedBinary(n-1) << (len(bin(n))-2)) + n) % 1000000007
+    
+    def concatenatedBinaryIter(self, n: int) -> int:
+        ans = 1
+        for i in range(2, n+1):
+            ans = ((ans << (len(bin(i))-2)) + i) % 1000000007
+        return ans
