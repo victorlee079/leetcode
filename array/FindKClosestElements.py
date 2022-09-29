857a44039d5b1d216539e9b109c81cc70af32f7b
Victor Lee <victorlee079@gmail.com>
2022-09-29 10:59:18 +0800

    Update FindKClosestElements.py

--- array/FindKClosestElements.py
+++ array/FindKClosestElements.py
@@ -11,11 +11,12 @@ class Solution:
                     heapq.heappushpop(heap, (-abs(num-x), num))
         # k log k
         return sorted([item[1] for item in heap])
-                
+    
     def findClosestElementsBinarySearch(self, arr: List[int], k: int, x: int) -> List[int]:
         left, right = 0, len(arr) - k
         while left < right:
             mid = (left + right) // 2
+            # Sliding window
             if x - arr[mid] > arr[mid + k] - x:
                 left = mid + 1
             else:
