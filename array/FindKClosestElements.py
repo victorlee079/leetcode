5c9bf8778865f32a6ea82bb066c9ee145b5fb27c
Victor Lee <victorlee079@gmail.com>
2022-09-29 09:13:56 +0800

    Update FindKClosestElements.py

--- array/FindKClosestElements.py
+++ array/FindKClosestElements.py
@@ -1,6 +1,7 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         heap = []
+        # n log k
         for num in arr:
             if len(heap) < k:
                 heapq.heappush(heap, (-abs(num-x), num))
@@ -8,5 +9,6 @@ class Solution:
                 diff = -heap[0][0]
                 if abs(num-x) < diff:
                     heapq.heappushpop(heap, (-abs(num-x), num))
+        # k log k
         return sorted([item[1] for item in heap])
                 
