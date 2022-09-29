b61e0ed96f673ed28609d17998131e668b351318
Victor Lee <victorlee079@gmail.com>
2022-09-29 10:38:30 +0800

    Update FindKClosestElements.py

--- array/FindKClosestElements.py
+++ array/FindKClosestElements.py
@@ -12,3 +12,12 @@ class Solution:
         # k log k
         return sorted([item[1] for item in heap])
                 
+    def findClosestElementsBinarySearch(self, arr: List[int], k: int, x: int) -> List[int]:
+        left, right = 0, len(arr) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        return arr[left:left + k]
