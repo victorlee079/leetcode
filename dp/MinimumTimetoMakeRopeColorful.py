5f9fe4c4ebda70af23c16ea6990b92ace174ba9f
Victor Lee <victorlee079@gmail.com>
2022-10-03 11:15:23 +0800

    Create MinimumTimetoMakeRopeColorful.py

---
+++ dp/MinimumTimetoMakeRopeColorful.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        n = len(colors)
+        dp = [None] * n
+        dp[0] = (0, neededTime[0])
+
+        for i in range(1, n):
+            if colors[i] == colors[i-1]:
+                if neededTime[i] <= dp[i-1][1]:
+                    dp[i] = (dp[i-1][0] + neededTime[i], dp[i-1][1])
+                else:
+                    dp[i] = (dp[i-1][0] + dp[i-1][1], neededTime[i])
+            else:
+                dp[i] = (dp[i-1][0], neededTime[i])
+        
+        return dp[-1][0]
