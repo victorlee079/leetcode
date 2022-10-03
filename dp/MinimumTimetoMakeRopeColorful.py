9f143ac6fee3dad10d58ad6fb067ac446ac11e51
Victor Lee <victorlee079@gmail.com>
2022-10-03 11:26:29 +0800

    Update MinimumTimetoMakeRopeColorful.py

--- dp/MinimumTimetoMakeRopeColorful.py
+++ dp/MinimumTimetoMakeRopeColorful.py
@@ -14,3 +14,21 @@ class Solution:
                 dp[i] = (dp[i-1][0], neededTime[i])
         
         return dp[-1][0]
+
+    def minCostSpaceOpt(self, colors: str, neededTime: List[int]) -> int:
+        n = len(colors)
+        res, maxTime = 0, neededTime[0]
+
+        for i in range(1, n):
+            if colors[i] == colors[i-1]:
+                if neededTime[i] <= maxTime:
+                    res += neededTime[i]
+                else:
+                    res += maxTime
+                    maxTime = neededTime[i]
+            else:
+                maxTime = neededTime[i]    
+        
+        return res
+                
+            
