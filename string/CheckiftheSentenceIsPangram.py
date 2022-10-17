55223c42f84d7c2f9a014b4a4a827f0a2f462458
Victor Lee <victorlee079@gmail.com>
2022-10-17 09:11:18 +0800

    Update CheckiftheSentenceIsPangram.py

--- string/CheckiftheSentenceIsPangram.py
+++ string/CheckiftheSentenceIsPangram.py
@@ -7,3 +7,9 @@ class Solution:
             if letters[i] == 0:
                 return False
         return True
+
+    def checkIfPangramSet(self, sentence: str) -> bool:
+        letters = set()
+        for c in sentence:
+            letters.add(c)
+        return len(letters) == 26
