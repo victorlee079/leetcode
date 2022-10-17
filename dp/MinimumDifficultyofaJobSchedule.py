bd81f067ef332fa988563677bdcb512b5bbb66ef
Victor Lee <victorlee079@gmail.com>
2022-10-17 12:41:52 +0800

    Create MinimumDifficultyofaJobSchedule.py

---
+++ dp/MinimumDifficultyofaJobSchedule.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        
+        n = len(jobDifficulty)
+
+        if n < d:
+            return -1
+
+        @lru_cache(None)
+        def dfs(i, d):
+            if d == 1:
+                return max(jobDifficulty[i:])
+            
+            res, maxd = math.inf, 0
+            for j in range(i, n-d+1):
+                maxd = max(maxd, jobDifficulty[j])
+                res = min(res, maxd + dfs(j+1, d-1))
+            return res
+        return dfs(0, d)
