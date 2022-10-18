10fcd2168cc97de5deda29b0dc32e4214879412d
Victor Lee <victorlee079@gmail.com>
2022-10-18 09:44:43 +0800

    Update MinimumDifficultyofaJobSchedule.py

--- dp/MinimumDifficultyofaJobSchedule.py
+++ dp/MinimumDifficultyofaJobSchedule.py
@@ -17,3 +17,30 @@ class Solution:
                 res = min(res, maxd + dfs(j+1, d-1))
             return res
         return dfs(0, d)
+    
+    # O(nnd)
+    def minDifficultyBottomUp(self, jobDifficulty: List[int], d: int) -> int:
+        if d > len(jobDifficulty):
+            return -1
+
+        n = len(jobDifficulty)
+        dp = [[0 for a in range(n)] for b in range(d)]
+        
+        # dp[i][j] = min difficult for day i and job j
+        currMax = jobDifficulty[0]
+        for i in range(n-d+1):
+            currMax = max(currMax, jobDifficulty[i])
+            dp[0][i] = currMax
+
+        for i in range(1, d):
+            for j in range(i, n-d+1+i):
+                currMax = jobDifficulty[j]
+                dp[i][j] = math.inf
+                # for job 1..j with day at i
+                # iterate all possible combination i..j (i --> min tasks required to fill first i days)
+                # min(dp[i][j], dp[i-1][r-1] + max(jobDifficulty[r:j+1]))
+                for r in range(j, i-1, -1):
+                    currMax = max(currMax, jobDifficulty[r])
+                    dp[i][j] = min(dp[i][j], dp[i-1][r-1] + currMax)
+
+        return dp[-1][-1]
