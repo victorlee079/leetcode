b8cb6d944a3520934896555bbeb7a9183a57b7ec
Victor Lee <victorlee079@gmail.com>
2022-10-23 13:56:30 +0800

    Create SetMismatch.py

---
+++ hashtable/SetMismatch.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        arr = [0] * n
+        for num in nums:
+            arr[num-1] += 1
+        twice = None
+        missing = None
+        for i in range(n):
+            if arr[i] > 1:
+                twice = i+1
+            if arr[i] == 0:
+                missing = i+1
+            if twice and missing:
+                break
+        return [twice, missing]
