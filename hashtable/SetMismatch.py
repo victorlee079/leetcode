28bcbc8790813228bb875decf002746d1b7ab7f6
Victor Lee <victorlee079@gmail.com>
2022-10-23 14:00:41 +0800

    Update SetMismatch.py

--- hashtable/SetMismatch.py
+++ hashtable/SetMismatch.py
@@ -14,3 +14,8 @@ class Solution:
             if twice and missing:
                 break
         return [twice, missing]
+
+    def findErrorNumsMath(self, nums: List[int]) -> List[int]:
+        n, a, b = len(nums), sum(nums), sum(set(nums))
+        s = n * (n+1) // 2
+        return [a-b, s-b]
