1931aaab2b08d068d690739069146aa51cfa0919
Victor Lee <victorlee079@gmail.com>
2022-10-28 09:13:09 +0800

    Update GroupAnagrams.py

--- hashtable/GroupAnagrams.py
+++ hashtable/GroupAnagrams.py
@@ -5,3 +5,17 @@ class Solution:
             t = "".join(sorted(s))
             d[t].append(s)
         return d.values()
+
+    def count(self, str):
+        arr = [0] * 26
+        for c in str:
+            arr[ord(c) - ord('a')] += 1
+        return ''.join([chr(s) for s in arr])
+
+
+    def groupAnagramsCount(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+        for s in strs:
+            k = self.count(s)
+            d[k].append(s)
+        return d.values()
