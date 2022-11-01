4839af0d470fdf84efac66850d669c2643db3a1f
Victor Lee <victorlee079@gmail.com>
2022-11-01 14:12:00 +0800

    Update WhereWilltheBallFall.py

--- simulation/WhereWilltheBallFall.py
+++ simulation/WhereWilltheBallFall.py
@@ -17,3 +17,21 @@ class Solution:
         for k, j in dp:
             ret[k] = j
         return ret
+
+    # Botton Up DP
+    def findBallDp(self, grid: List[List[int]]) -> List[int]:
+        m, n = len(grid), len(grid[0])
+        dp = [[0] * n for _ in range(m+1)]
+        for i in range(m, -1, -1):
+            for j in range(n):
+                if i == m:
+                    dp[i][j] = j
+                    continue
+
+                next_column = j + grid[i][j]
+                if next_column < 0 or next_column > n-1 or grid[i][j] != grid[i][next_column]:
+                    dp[i][j] = -1
+                else:
+                    dp[i][j] = dp[i+1][next_column]
+        
+        return dp[0]
