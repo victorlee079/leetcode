14c664d21ca098ff79cf39523b75b0b23449fe6d
Victor Lee <victorlee079@gmail.com>
2022-11-02 09:06:53 +0800

    Update MinimumGeneticMutation.py

--- bfs/MinimumGeneticMutation.py
+++ bfs/MinimumGeneticMutation.py
@@ -35,3 +35,24 @@ class Solution:
                             visited.add(b)
 
         return -1
+    
+    def minMutation2(self, start: str, end: str, bank: List[str]) -> int:
+        if end not in bank:
+            return -1
+
+        q, bank = deque([start]), set(bank)
+        ret = 0
+        while q:
+            n = len(q)
+            ret += 1
+            for i in range(n):
+                s = q.popleft()
+                for c in 'ACGT':
+                    for j in range(len(s)):
+                        next_mut = s[:j] + c + s[j+1:]
+                        if next_mut in bank:
+                            if next_mut == end:
+                                return ret
+                            q.append(next_mut)
+                            bank.remove(next_mut)
+        return -1
