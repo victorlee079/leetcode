11a83ec6d8b2f1a5594c972ce319af5efb238b2c
Victor Lee <victorlee079@gmail.com>
2022-11-04 11:36:43 +0800

    Update RotatedDigits.py

--- math/RotatedDigits.py
+++ math/RotatedDigits.py
@@ -8,3 +8,14 @@ class Solution:
             if s.issubset(valid) and not s.issubset(ng):
                 ret += 1
         return ret
+    
+    def rotatedDigits2(self, N: int) -> int:
+        count = 0
+        for d in range(1, N+1):
+            # O(log n)
+            d = str(d)
+            if '3' in d or '4' in d or '7' in d:
+                continue
+            if '2' in d or '5' in d or '6' in d or '9' in d:
+                count+=1
+        return count
