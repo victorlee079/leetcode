4a17a2da6783d6b23e3ab43a77147631c8aa1230
Victor Lee <victorlee079@gmail.com>
2022-11-11 09:11:11 +0800

    Update RemoveDuplicatesFromSortedArray.py

--- twopointer/RemoveDuplicatesFromSortedArray.py
+++ twopointer/RemoveDuplicatesFromSortedArray.py
@@ -1,9 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        slow = fast = 1
+        slow = 1
         for i in range(1, len(nums)):
-            if nums[slow-1] != nums[fast]:
-                nums[slow] = nums[fast]
+            if nums[slow-1] != nums[i]:
+                nums[slow] = nums[i]
                 slow += 1
-            fast += 1
         return slow
