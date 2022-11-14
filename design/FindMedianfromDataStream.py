4c1266348eadc073ce3868450ac588dfce3ca1c6
Victor Lee <victorlee079@gmail.com>
2022-11-14 12:45:46 +0800

    Update FindMedianfromDataStream.py

--- design/FindMedianfromDataStream.py
+++ design/FindMedianfromDataStream.py
@@ -21,3 +21,20 @@ class MedianFinder:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+
+class MedianFinderV2:
+    def __init__(self):
+        self.small = []  # the smaller half of the list, max heap (invert min-heap)
+        self.large = []  # the larger half of the list, min heap
+
+    def addNum(self, num):
+        if len(self.small) == len(self.large):
+            heappush(self.large, -heappushpop(self.small, -num))
+        else:
+            heappush(self.small, -heappushpop(self.large, num))
+
+    def findMedian(self):
+        if len(self.small) == len(self.large):
+            return float(self.large[0] - self.small[0]) / 2.0
+        else:
+            return float(self.large[0])
