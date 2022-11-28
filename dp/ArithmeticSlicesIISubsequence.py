e0262e904a243b2ce4a2c076467c0ff87fdeb7ca
Victor Lee <victorlee079@gmail.com>
2022-11-28 15:11:09 +0800

    Update ArithmeticSlicesIISubsequence.py

--- dp/ArithmeticSlicesIISubsequence.py
+++ dp/ArithmeticSlicesIISubsequence.py
@@ -6,6 +6,7 @@ class Solution:
         for i in range(n):
             for j in range(i):
                 diff = nums[i] - nums[j]
+                # cnt[j][diff] contains the count of delta
                 s = cnt[j][diff] if diff in cnt[j] else 0
                 if diff not in cnt[i]:
                     cnt[i][diff] = 0
