f6305a3ff37f65370a2f21841cb315506596c664
Victor Lee <victorlee079@gmail.com>
2022-12-21 11:52:22 +0800

    Update PossibleBipartition.py

--- bfs/PossibleBipartition.py
+++ bfs/PossibleBipartition.py
@@ -34,3 +34,31 @@ class Solution:
                 return False
 
         return True
+
+    def possibleBipartitionUnionFind(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        
+        for a, b in dislikes:
+            graph[a-1].append(b-1)
+            graph[b-1].append(a-1)
+
+        parents = [i for i in range(n)]
+
+        def find(i):
+            if i != parents[i]:
+                parents[i] = find(parents[i])
+            return parents[i]
+
+        def union(i, p):
+            parents[find(i)] = p
+
+        for i in range(n):
+            i_parent = find(i)
+            if i in graph:
+                first_parent = find(graph[i][0])
+                for j in graph[i][1:]:
+                    union(j, first_parent)
+                    if i_parent == find(j):
+                        return False
+
+        return True
