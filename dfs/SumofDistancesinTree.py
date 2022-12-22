9e43755321f4581f3c1aee50f82a0b035f001fa1
Victor Lee <victorlee079@gmail.com>
2022-12-22 15:35:23 +0800

    Update SumofDistancesinTree.py

--- dfs/SumofDistancesinTree.py
+++ dfs/SumofDistancesinTree.py
@@ -7,19 +7,24 @@ class Solution(object):
 
         count = [1] * N
         ans = [0] * N
-        def dfs(node = 0, parent = None):
+
+        # This only gives the ans for root
+        def dfs(node, parent):
             for child in graph[node]:
                 if child != parent:
                     dfs(child, node)
                     count[node] += count[child]
+                    # One level up -> distances of the nodes of the child increases by 1
                     ans[node] += ans[child] + count[child]
 
-        def dfs2(node = 0, parent = None):
+        def dfs2(node, parent):
             for child in graph[node]:
                 if child != parent:
-                    ans[child] = ans[node] - count[child] + N - count[child]
+                    # top down
+                    # ans[x] - ans[y] = counts[y] - counts[x]
+                    ans[child] = ans[node] + (N - count[child]) - count[child] + N
                     dfs2(child, node)
 
-        dfs()
-        dfs2()
+        dfs(0, None)
+        dfs2(0, None)
         return ans
