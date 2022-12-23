a5eac54f555177484bf1e2c10cb910f032726f36
Victor Lee <victorlee079@gmail.com>
2022-12-23 11:15:37 +0800

    Update BestTimetoBuyandSellStockwithCooldown.py

--- dp/BestTimetoBuyandSellStockwithCooldown.py
+++ dp/BestTimetoBuyandSellStockwithCooldown.py
@@ -7,13 +7,17 @@ class Solution:
             buy[i] = max(max(sell[:i-1]) - prices[i] if i > 1 else - prices[i], -prices[i])
             sell[i] = max(buy[:i]) + prices[i] if i > 0 else 0
         return max(sell)
-      
-    def maxProfitBetter(self, prices: List[int]) -> int:
+    
+    def maxProfit0Space(self, prices: List[int]) -> int:
         n = len(prices)
         hold, sell, rest = -inf, 0, 0
         for i in range(n):
             prev = sell
+            # Sell if holding
             sell = hold + prices[i]
+            # Buy after cooldown or keep hold
             hold = max(hold, rest - prices[i])
+            # Keep cooldown or change to cooldown
             rest = max(rest, prev)
         return max(rest, sell)
+
