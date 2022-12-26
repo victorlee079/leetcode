49d6ab1b3a55a1f1feb0a56db6b254374957c0c3
Victor Lee <victorlee079@gmail.com>
2022-12-26 11:19:01 +0800

    Update CanJump.py

--- dp/CanJump.py
+++ dp/CanJump.py
@@ -1,11 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        maxPos = 0
-        i = 0
-        while i <= maxPos:
-            maxPos = max(maxPos, i + nums[i])
-            if maxPos >= n - 1: return True
-            i += 1
-
-        return False
+        lp = n-1
+        for i in range(n-2, -1, -1):
+            if i + nums[i] >= lp:
+                lp = i
+        return lp == 0
