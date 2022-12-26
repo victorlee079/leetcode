cc9c23c77af37a2350cf2b954d27c6693d8d69a5
Victor Lee <victorlee079@gmail.com>
2022-12-26 11:03:51 +0800

    Update DominoandTrominoTiling.py

--- dp/DominoandTrominoTiling.py
+++ dp/DominoandTrominoTiling.py
@@ -4,6 +4,9 @@ class Solution:
             dp[0] = dp[1] = 1
             dp[2] = 2
             dp[3] = 5
+            # Each dp[i] = dp[i-1] + dp[i-2] + 2 * (dp[i-3] + ... + dp[0])
+            # dp[i-1] + dp[i-3] + dp[i-2] + dp[i-3] + 2 * (dp[i-4] + ... + dp[0])
+            # 2 * dp[i-1] + dp[i-3]
             for i in range(4, n+1):
                 dp[i] = 2 * dp[i-1] + dp[i-3]
             return dp[n] % 1000000007
