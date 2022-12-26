d2f7d56ac6161de964b9efc68bf9430682dcc509
Victor Lee <victorlee079@gmail.com>
2022-12-26 11:01:31 +0800

    Create DominoandTrominoTiling.py

---
+++ dp/DominoandTrominoTiling.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numTilings(self, n):
+            dp = [0] * (1001)
+            dp[0] = dp[1] = 1
+            dp[2] = 2
+            dp[3] = 5
+            for i in range(4, n+1):
+                dp[i] = 2 * dp[i-1] + dp[i-3]
+            return dp[n] % 1000000007
+
+s = Solution()
+print(s.numTilings(3))
