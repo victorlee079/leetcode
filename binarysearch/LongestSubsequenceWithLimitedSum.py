e3f8737c94740870f6d0f2138ecce2e29a813b10
Victor Lee <victorlee079@gmail.com>
2022-12-26 12:37:08 +0800

    Create LongestSubsequenceWithLimitedSum.py

---
+++ binarysearch/LongestSubsequenceWithLimitedSum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        n = len(nums)
+        nums = sorted(nums)
+        for i in range(1, n):
+            nums[i] += nums[i-1]
+        
+        def search(val):
+            l, r = 0, n
+            while l < r:
+                mid = l + (r - l) // 2
+                if nums[mid] <= val:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l
+        
+        ans = [0] * len(queries)
+        for i in range(len(queries)):
+            ans[i] = search(queries[i])
+        return ans
+            
