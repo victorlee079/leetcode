c48bdfc122257cb3f06e58bf82f21e9597315d45
Victor Lee <victorlee079@gmail.com>
2022-12-28 10:14:49 +0800

    Create RemoveStonestoMinimizetheTotal.py

---
+++ heap/RemoveStonestoMinimizetheTotal.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        ans, min_heap = 0, []
+        # O(k + n log k)
+        for pile in piles:
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, pile)
+            else:
+                # Remaining items: k < len(piles)
+                ans += heapq.heappushpop(min_heap, pile)
+
+        n = len(min_heap)
+
+        # Convert to max_heap to pop the largest
+        # O(k log k)
+        max_heap = []
+        for p in min_heap:
+            heapq.heappush(max_heap, -p)
+
+        # Apply k operations to each element
+        # O(k log k)
+        for _ in range(k):
+            p = -heapq.heappop(max_heap)
+            p = p - p // 2
+            # Push back for k > len(piles)
+            heapq.heappush(max_heap, -p)
+
+        # negative for max heap in python
+        return ans - sum(max_heap)
+        
+    def minStoneSumSimpler(self, piles: List[int], k: int) -> int:
+        piles = [-p for p in piles]
+        heapq.heapify(piles)
+        for _ in range(k):
+            curr = -heapq.heappop(piles)
+            remove = curr // 2
+            heapq.heappush(piles, -(curr - remove))
+        return -sum(piles)
