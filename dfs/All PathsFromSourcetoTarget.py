730ba0592f0d2bf9584d388844e6861dfeaa49a4
Victor Lee <victorlee079@gmail.com>
2022-12-30 08:39:01 +0800

    Create All PathsFromSourcetoTarget.py

---
+++ dfs/All PathsFromSourcetoTarget.py
@@ -0,0 +1,16 @@
+class Solution:
+    def allPathsSourceTarget(self, graph):
+        n = len(graph)
+        ret = []
+        def dfs(path, i):
+            if i == n-1:
+                ret.append(path[::])
+                return
+            
+            for node in graph[i]:
+                path.append(node)
+                dfs(path, node)
+                path.pop()
+        
+        dfs([0], 0)
+        return ret
