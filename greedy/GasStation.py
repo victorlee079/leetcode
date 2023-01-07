80424532fd15de2a2651900cf5dd314aaab8987e
Victor Lee <victorlee079@gmail.com>
2023-01-07 22:12:38 +0800

    Update GasStation.py

--- greedy/GasStation.py
+++ greedy/GasStation.py
@@ -10,3 +10,18 @@ class Solution:
         if total < 0:
             return -1
         return start
+
+    def canCompleteCircuit20230107(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        cum, total, ans = 0, 0, -1
+        for i in range(n):
+            diff = gas[i] - cost[i]
+            cum += diff
+            if cum < 0:
+                ans = -1
+                cum = 0
+            else:
+                if ans == -1:
+                    ans = i
+            total += diff
+        return ans if total > -1 else -1
