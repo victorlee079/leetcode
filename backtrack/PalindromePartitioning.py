c250578a604c8f2d97c2dff4ab3fe20d72158ebe
Victor Lee <victorlee079@gmail.com>
2023-01-22 23:19:28 +0800

    Update PalindromePartitioning.py

--- backtrack/PalindromePartitioning.py
+++ backtrack/PalindromePartitioning.py
@@ -23,3 +23,46 @@ class Solution:
         ans = []
         backtrack([], 0, ans)
         return ans
+
+    def partitionLru(self, s: str) -> List[List[str]]:
+        n = len(s)
+        ans = []
+
+        @lru_cache
+        def isPalindrome(start, end):
+            if start > end:
+                return True
+            if s[start] == s[end]:
+                return isPalindrome(start+1, end-1)
+            return False
+
+        def backtrack(path, index):
+            if index == n:
+                ans.append(path[::])
+                return
+            for i in range(index, n):
+                if isPalindrome(index, i):
+                    path.append(s[index:i+1])
+                    backtrack(path, i+1)
+                    path.pop()
+        backtrack([], 0)
+        return ans
+
+    def partitionDp(self, s):
+        dp = [[False] * len(s) for _ in range(len(s))]
+
+        def backtrack(path, i, ret):
+            if i == len(s):
+                ret.append(path[::])
+                return
+
+            for j in range(i, len(s)):
+                if s[i] == s[j] and (j - i <= 2 or dp[i+1][j-1]):
+                    dp[i][j] = True
+                    path.append(s[i:j+1])
+                    backtrack(path, j+1, ret)
+                    path.pop()
+
+        ans = []
+        backtrack([], 0, ans)
+        return ans
