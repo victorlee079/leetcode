4783137a4c459c65dd42f0ac74fafb08e40dd633
Victor Lee <victorlee079@gmail.com>
2023-01-29 23:39:06 +0800

    Update CheapestFlightsWithinKStops.py

--- bfs/CheapestFlightsWithinKStops.py
+++ bfs/CheapestFlightsWithinKStops.py
@@ -19,3 +19,14 @@ class Solution:
             k -= 1
 
         return costs[dst] if costs[dst] != inf else -1
+
+    def findCheapestPriceBellmanFord(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        dist = [inf] * n
+        dist[src] = 0
+        for i in range(k+1):
+            temp = dist.copy()
+            for origin, dest, price in flights:
+                if dist[origin] != inf:
+                    temp[dest] = min(temp[dest], dist[origin] + price)
+            dist = temp
+        return dist[dst] if dist[dst] != inf else -1
