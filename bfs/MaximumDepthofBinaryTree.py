28037d260bf611331368a118962116afe436dff3
Victor Lee <victorlee079@gmail.com>
2023-02-16 10:33:56 +0000

    Create MaximumDepthofBinaryTree.py

---
+++ bfs/MaximumDepthofBinaryTree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        q = deque()
+        q.append(root)
+        depth = 0
+        while q:
+            depth += 1
+            size = len(q)
+            for _ in range(size):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return depth
