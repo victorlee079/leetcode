44a83e3b53250cc7192d16667920c35510b05319
Victor Lee <victorlee079@gmail.com>
2023-03-22 23:42:27 +0000

    Update MinimumScoreofaPathBetweenTwoCities.py

--- dfs/MinimumScoreofaPathBetweenTwoCities.py
+++ dfs/MinimumScoreofaPathBetweenTwoCities.py
@@ -17,3 +17,33 @@ class Solution:
 
         dfs(1)
         return self.ans
+
+    def minScoreUf(self, n: int, roads: List[List[int]]) -> int:
+        parents = [i for i in range(n+1)]
+        ranks = [0] * (n+1)
+
+        def find(i):
+            if parents[i] != i:
+                i = find(parents[i])
+            return i
+
+        def union(a, b):
+            x, y = find(a), find(b)
+            if x == y:
+                return
+            elif ranks[x] < ranks[y]:
+                parents[x] = y
+            elif ranks[x] > ranks[y]:
+                parents[y] = x
+            else:
+                parents[y] = x
+                ranks[x] += 1
+
+        for a, b, distance in roads:
+            union(a, b)
+
+        ans = math.inf
+        for a, b, distance in roads:
+            if find(1) == find(a):
+                ans = min(ans, distance)
+        return ans
