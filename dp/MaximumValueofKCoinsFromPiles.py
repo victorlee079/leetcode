1853f5510258fabea7c5f73a57d89036719b864a
Victor Lee <victorlee079@gmail.com>
2023-04-15 22:02:14 +0100

    Update MaximumValueofKCoinsFromPiles.py

--- dp/MaximumValueofKCoinsFromPiles.py
+++ dp/MaximumValueofKCoinsFromPiles.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
         n = len(piles)
+        for pile in piles:
+            for i in range(1, len(pile)):
+                pile[i] += pile[i-1]
         dp = [[0] * (k + 1) for i in range(n + 1)]
         for i in range(1, n + 1):
             for coins in range(0, k + 1):
-                current_sum = 0
                 for current_coins in range(0, min(len(piles[i - 1]), coins) + 1):
-                    if current_coins > 0:
-                        current_sum += piles[i - 1][current_coins - 1]
+                    current_sum = piles[i - 1][current_coins -
+                                               1] if current_coins > 0 else 0
                     dp[i][coins] = max(dp[i][coins],
                                        dp[i - 1][coins - current_coins] + current_sum)
         return dp[n][k]
