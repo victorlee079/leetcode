c32322927715313e604117e9884ca63646a5ca12
Victor Lee <victorlee079@gmail.com>
2023-04-20 22:50:39 +0100

    Update MaximumWidthofBinaryTree.py

--- bfs/MaximumWidthofBinaryTree.py
+++ bfs/MaximumWidthofBinaryTree.py
@@ -25,3 +25,19 @@ class Solution:
                     nq.append((node.right, 2*index+1))
             return bfs(max(width, end-start+1), nq)
         return bfs(0, q)
+
+    def widthOfBinaryTreeIterative(self, root: Optional[TreeNode]) -> int:
+        q = deque([(1, root)])
+        ans = 1
+        while q:
+            n = len(q)
+            left = right = q[0][0]
+            for _ in range(n):
+                i, node = q.popleft()
+                right = max(right, i)
+                if node.left:
+                    q.append((2 * i - 1, node.left))
+                if node.right:
+                    q.append((2 * i, node.right))
+            ans = max(right - left + 1, ans)
+        return ans
