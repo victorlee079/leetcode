f478c68a619779eaf5b7b2698b4848dd8c9847f9
Victor Lee <victorlee079@gmail.com>
2023-05-12 13:47:33 +0100

    Update SolvingQuestionsWithBrainpower.py

--- dp/SolvingQuestionsWithBrainpower.py
+++ dp/SolvingQuestionsWithBrainpower.py
@@ -8,3 +8,15 @@ class Solution:
             return max(points + helper(i + brainpower + 1), helper(i + 1))
 
         return helper(0)
+
+    def mostPointsIter(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        dp = [0] * n
+        dp[-1] = questions[-1][0]
+        for i in range(n - 2, -1, -1):
+            points, brainpower = questions[i]
+            dp[i] = points
+            if i + brainpower + 1 < n:
+                dp[i] += dp[i + brainpower + 1]
+            dp[i] = max(dp[i], dp[i + 1])
+        return dp[0]
